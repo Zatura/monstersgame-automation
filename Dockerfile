53289cff876024cdf6898e0bc555df1f7ba789ca
zatura <xturax@gmail.com>
2020-08-13 23:50:24 -0300

    Storage class for s3 bucket manipulation

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY . .
 RUN apk update
 RUN apk add chromium chromium-chromedriver
 RUN pip install --upgrade pip
-RUN pip install selenium
+RUN pip install selenium boto3
 
 CMD ["python", "src/main.py"]
--- src/model/storage.py
+++ src/model/storage.py
@@ -1,11 +1,15 @@
 import logging
 import boto3
 from botocore.exceptions import ClientError
+from auth.s3 import region_name, aws_access_key_id, aws_access_key_secret
 
 
 class Storage:
     def __init__(self):
-        self.s3_client = boto3.client('s3')
+        self.s3_client = boto3.client('s3',
+                                      region_name=region_name,
+                                      aws_access_key_id=aws_access_key_id,
+                                      aws_secret_access_key=aws_access_key_secret)
 
     def create_bucket(self, bucket_name, region=None):
         try:
